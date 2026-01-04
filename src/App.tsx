f9a7b107a9a8fd27a0c4717c811b694791c37251
Tristan <tristanpinzari@example.com>
2026-01-03 21:42:15 -0500

    Blinker and 10 max errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,109 @@
 import { useState, useRef, useEffect, useMemo } from "react";
 import Racetrack from "./components/Racetrack";
 
+interface Writing {
+  text: string;
+  origin: string;
+  author: string;
+  uploader: string;
+}
+
+function getDisplayData(
+  input: string,
+  index: number,
+  writing: Writing,
+  textSplitted: string[]
+) {
+  const currentWord = textSplitted[index] || "";
+  const wordStartIndex =
+    index === 0 ? 0 : textSplitted.slice(0, index).join(" ").length + 1;
+  const wordEndIndex = wordStartIndex + currentWord.length;
+
+  let incorrectStartIndex = null;
+  for (let i = 0; i < Math.min(currentWord.length, input.length); i++) {
+    if (currentWord[i] !== input[i]) {
+      incorrectStartIndex = i;
+      break;
+    }
+  }
+  if (incorrectStartIndex == null && currentWord.length < input.length) {
+    incorrectStartIndex = currentWord.length;
+  }
+
+  let correctUnderline,
+    incorrectUnderline,
+    currentUntypedUnderline,
+    untypedUnderline,
+    incorrectWords,
+    untypedWords;
+  const correctWords = writing.text.slice(0, wordStartIndex);
+  const currentUntypedLetter =
+    input.length >= currentWord.length
+      ? writing.text[wordStartIndex + input.length] || ""
+      : "";
+
+  if (incorrectStartIndex == null) {
+    correctUnderline = writing.text.slice(
+      wordStartIndex,
+      wordStartIndex + input.length
+    );
+    incorrectUnderline = "";
+    currentUntypedUnderline =
+      wordStartIndex + input.length > wordEndIndex - 1
+        ? ""
+        : writing.text[wordStartIndex + input.length];
+    untypedUnderline = writing.text.slice(
+      wordStartIndex + input.length + 1,
+      wordEndIndex
+    );
+    incorrectWords = "";
+    untypedWords = writing.text.slice(wordEndIndex);
+  } else {
+    correctUnderline = writing.text.slice(
+      wordStartIndex,
+      wordStartIndex + incorrectStartIndex
+    );
+    incorrectUnderline = writing.text.slice(
+      wordStartIndex + incorrectStartIndex,
+      wordStartIndex + Math.min(currentWord.length, input.length)
+    );
+    currentUntypedUnderline =
+      wordStartIndex + input.length > wordEndIndex - 1
+        ? ""
+        : writing.text[wordStartIndex + input.length];
+    untypedUnderline = writing.text.slice(
+      wordStartIndex +
+        Math.min(currentWord.length, input.length) +
+        (currentUntypedUnderline.length > 0 ? 1 : 0),
+      wordEndIndex
+    );
+    incorrectWords =
+      input.length > currentWord.length
+        ? writing.text.slice(wordEndIndex, wordStartIndex + input.length)
+        : "";
+    untypedWords = writing.text.slice(
+      wordStartIndex +
+        Math.max(currentWord.length, input.length) +
+        (currentUntypedLetter.length > 0 ? 1 : 0)
+    );
+  }
+
+  const progress =
+    (correctWords.length + correctUnderline.length) / writing.text.length;
+
+  return {
+    correctWords,
+    correctUnderline,
+    incorrectUnderline,
+    currentUntypedUnderline,
+    untypedUnderline,
+    incorrectWords,
+    currentUntypedLetter,
+    untypedWords,
+    progress,
+  };
+}
+
 function App() {
   const [textBoxInput, setTextBoxInput] = useState("");
   const [wordIndex, setWordIndex] = useState(0);
@@ -8,10 +111,11 @@ function App() {
 
   const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
   const timeRef = useRef(0);
+  const startTimeRef = useRef(0);
   const totalCorrectCharsRef = useRef(0);
 
-  const [writing, setWriting] = useState({
-    text: "Betty decided to write a short story and she was sure it was going to be amazing.",
+  const [writing, setWriting] = useState<Writing>({
+    text: "BettyBettyBetty decided to write a short story.",
     origin: "Random text generator",
     author: "i don't know",
     uploader: "Me (Tristan)",
@@ -21,94 +125,13 @@ function App() {
     return writing.text.split(" ");
   }, [writing.text]);
 
-  function getDisplayData(input: string, index: number) {
-    const currentWord = textSplitted[index] || "";
-    const wordStartIndex =
-      index === 0 ? 0 : textSplitted.slice(0, index).join(" ").length + 1;
-    const wordEndIndex = wordStartIndex + currentWord.length;
-
-    let incorrectStartIndex = null;
-    for (let i = 0; i < Math.min(currentWord.length, input.length); i++) {
-      if (currentWord[i] !== input[i]) {
-        incorrectStartIndex = i;
-        break;
-      }
-    }
-    if (incorrectStartIndex == null && currentWord.length < input.length) {
-      incorrectStartIndex = currentWord.length;
-    }
-
-    let correctUnderline,
-      incorrectUnderline,
-      untypedUnderline,
-      incorrectWords,
-      untypedWords;
-    const correctWords = writing.text.slice(0, wordStartIndex);
-
-    if (incorrectStartIndex == null) {
-      correctUnderline = writing.text.slice(
-        wordStartIndex,
-        wordStartIndex + input.length
-      );
-      incorrectUnderline = "";
-      untypedUnderline = writing.text.slice(
-        wordStartIndex + input.length,
-        wordEndIndex
-      );
-      incorrectWords = "";
-      untypedWords = writing.text.slice(wordEndIndex);
-    } else {
-      correctUnderline = writing.text.slice(
-        wordStartIndex,
-        wordStartIndex + incorrectStartIndex
-      );
-      incorrectUnderline = writing.text.slice(
-        wordStartIndex + incorrectStartIndex,
-        wordStartIndex + Math.min(currentWord.length, input.length)
-      );
-      untypedUnderline = writing.text.slice(
-        wordStartIndex + Math.min(currentWord.length, input.length),
-        wordEndIndex
-      );
-      incorrectWords =
-        input.length > currentWord.length
-          ? writing.text.slice(wordEndIndex, wordStartIndex + input.length)
-          : "";
-      untypedWords = writing.text.slice(
-        wordStartIndex + Math.max(currentWord.length, input.length)
-      );
-    }
-
-    const progress =
-      (correctWords.length + correctUnderline.length) / writing.text.length;
-
-    if (progress === 1) {
-      return {
-        correctWords: writing.text,
-        correctUnderline: "",
-        incorrectUnderline: "",
-        untypedUnderline: "",
-        incorrectWords: "",
-        untypedWords: "",
-        progress: 1,
-      };
-    }
-
-    return {
-      correctWords,
-      correctUnderline,
-      incorrectUnderline,
-      untypedUnderline,
-      incorrectWords,
-      untypedWords,
-      progress,
-    };
-  }
-
-  const displayData = getDisplayData(textBoxInput, wordIndex);
+  const displayData = useMemo(() => {
+    return getDisplayData(textBoxInput, wordIndex, writing, textSplitted);
+  }, [textBoxInput, wordIndex, writing, textSplitted]);
 
   function startTimer() {
     if (timerRef.current) return;
+    startTimeRef.current = Date.now();
     timerRef.current = setInterval(() => {
       timeRef.current += 1;
       const minutes = (timeRef.current || 0) / 60;
@@ -122,6 +145,9 @@ function App() {
       clearInterval(timerRef.current);
       timerRef.current = null;
     }
+    const minutes = (Date.now() - startTimeRef.current) / 60000;
+    const newWPM = totalCorrectCharsRef.current / 5 / minutes;
+    setWPM(Math.round(newWPM));
   }
 
   function reset() {
@@ -165,10 +191,19 @@ function App() {
       setTextBoxInput("");
       setWordIndex(wordIndex + 1);
     } else {
-      const totalErrors =
-        displayData.incorrectWords.length +
-        displayData.incorrectUnderline.length;
-      if (totalErrors >= 10 && value.length > textBoxInput.length) {
+      let correctLetters = 0;
+      const currentWord = textSplitted[wordIndex];
+      for (let i = 0; i < value.length; i++) {
+        if (value[i] != currentWord[i]) {
+          correctLetters = i + 1;
+          break;
+        }
+        correctLetters += 1;
+      }
+      if (
+        value.length - correctLetters > 10 &&
+        value.length > textBoxInput.length
+      ) {
         return;
       }
       setTextBoxInput(value);
@@ -192,8 +227,24 @@ function App() {
             <span className="incorrect underline">
               {displayData.incorrectUnderline}
             </span>
+            <span
+              className={`${
+                displayData.currentUntypedUnderline != " " ? "underline" : ""
+              } ${
+                displayData.currentUntypedUnderline.length > 0 ? "blinker" : ""
+              }`}
+            >
+              {displayData.currentUntypedUnderline}
+            </span>
             <span className="underline">{displayData.untypedUnderline}</span>
             <span className="incorrect">{displayData.incorrectWords}</span>
+            <span
+              className={
+                displayData.currentUntypedLetter.length > 0 ? "blinker" : ""
+              }
+            >
+              {displayData.currentUntypedLetter}
+            </span>
             <span>{displayData.untypedWords}</span>
           </p>
           <input
